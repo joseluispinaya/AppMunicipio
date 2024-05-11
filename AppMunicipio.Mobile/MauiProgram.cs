3f907d0c4656a8f52428e44c36dc3d2d071b6ea8
JOSE LUIS PINAYA BALDERRAMA <jpinayab@doc.emi.edu.bo>
2024-05-10 21:56:48 -0400

    errores

--- AppMunicipio.Mobile/MauiProgram.cs
+++ AppMunicipio.Mobile/MauiProgram.cs
@@ -4,6 +4,15 @@ using AppMunicipio.Mobile.ViewModels.Startup;
 using AppMunicipio.Mobile.Views.Dashboard;
 using AppMunicipio.Mobile.Views.Startup;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Auth;
+using Plugin.Firebase.Shared;
+#if IOS
+using Plugin.Firebase.iOS;
+#endif
+#if ANDROID
+using Plugin.Firebase.Android;
+#endif
 
 namespace AppMunicipio.Mobile
 {
@@ -14,6 +23,7 @@ namespace AppMunicipio.Mobile
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .RegisterFirebaseServices()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -42,6 +52,28 @@ namespace AppMunicipio.Mobile
 #endif
 
             return builder.Build();
+        }
+        private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+        {
+            builder.ConfigureLifecycleEvents(events =>
+            {
+#if IOS
+            events.AddiOS(iOS => iOS.FinishedLaunching((app, launchOptions) => {
+                CrossFirebase.Initialize(app, launchOptions, CreateCrossFirebaseSettings());
+                return false;
+            }));
+#else
+                events.AddAndroid(android => android.OnCreate((activity, state) =>
+                    CrossFirebase.Initialize(activity, state, CreateCrossFirebaseSettings())));
+#endif
+            });
+            builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+
+            return builder;
+        }
+        private static CrossFirebaseSettings CreateCrossFirebaseSettings()
+        {
+            return new CrossFirebaseSettings(isAuthEnabled: true);
         }
     }
 }
